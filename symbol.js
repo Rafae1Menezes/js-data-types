2ebea02a4928ce55e5bd91c2638b0f1461720837
Rafael Menezes <menezes.inbox@gmail.com>
2022-12-11 10:58:24 -0300

    feat: finaliza symbols

--- symbol.js
+++ symbol.js
@@ -42,12 +42,76 @@ class MyDate {
   constructor(...args) {
     this[kItens] = args.map((arg) => new Date(...arg));
   }
+
+  [Symbol.toPrimitive](coercionType) {
+    if (coercionType !== "string") throw new TypeError();
+
+    const items = this[kItens].map((item) =>
+      new Intl.DateTimeFormat("pt-BR", {
+        month: "long",
+        day: "2-digit",
+        year: "numeric",
+      }).format(item)
+    );
+
+    //retornando com o "e"
+    return new Intl.ListFormat("pt-BR", {
+      style: "long",
+      type: "conjunction",
+    }).format(items);
+  }
+
+  *[Symbol.iterator]() {
+    for (const item of this[kItens]) {
+      yield item;
+    }
+  }
+
+  async *[Symbol.asyncIterator]() {
+    const timeout = (ms) => new Promise((r) => setTimeout(r, ms));
+    for (const item of this[kItens]) {
+      await timeout(100);
+      yield item.toISOString();
+    }
+  }
+
+  //to String tag, para alterar o nome [object Object]
+  // Precis do gat pq foi herdado de do prototype Object
+  get [Symbol.toStringTag]() {
+    return "WHATS?";
+  }
 }
 
 const myDate = new MyDate([2021, 10, 03], [2020, 09, 04]);
 
+const expecteDates = [new Date(2021, 10, 03), new Date(2020, 9, 04)];
+
 // console.log(myDate[kItens]);
-assert.deepEqual(myDate[kItens], [
-  new Date(2021, 10, 03),
-  new Date(2020, 9, 04),
-]);
+assert.deepEqual(myDate[kItens], expecteDates);
+
+assert.deepStrictEqual(
+  Object.prototype.toString.call(myDate),
+  "[object WHATS?]"
+);
+
+assert.throws(() => myDate + 1, TypeError);
+
+//cerção explicita para char o toPrimite
+assert.deepStrictEqual(
+  String(myDate),
+  "03 de novembro de 2021 e 04 de outubro de 2020"
+);
+
+// implementar o iterator!
+assert.deepStrictEqual([...myDate], expecteDates);
+
+// (async () => {
+//   for await (const item of myDate) {
+//     console.log("assync interator", item);
+//   }
+// })();
+
+(async () => {
+  const dates = await Promise.all([...myDate]);
+  assert.deepStrictEqual(dates, expecteDates);
+})();
